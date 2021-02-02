6daee9aa460d40b0f69b68d5ab94f5db09e790bb
ortiz92105@gmail.com <ortiz92105@gmail.com>
2021-02-01 23:31:56 -0800

    Add project files.

---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+   {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "mary@test.com"},
+            new Employee() { Id = 1, Name = "James", Department = "HR", Email = "james@test.com" },
+            new Employee() { Id = 1, Name = "Don", Department = "IT", Email = "don@test.com" }
+            };
+        }
+            public Employee GetEmployee(int Id)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
